cae141cdc4e20bfe478bffbc4cbcfaede3fd5345
user141958 <user141958@eu234.local>
2018-08-23 10:03:38 +0100

    final

--- Unit Converstion/ViewController.m
+++ Unit Converstion/ViewController.m
@@ -8,11 +8,53 @@
 
 #import "ViewController.h"
 
+double convertUnitOneToUnitTwo (double unitOneValue){
+    double unitTwoValue;
+    unitTwoValue = unitOneValue*1000;
+    return unitTwoValue;
+}
+
+double convertUnitOneToUnitThree (double unitOneValue){
+    double unitThreeValue;
+    unitThreeValue = unitOneValue*3281;
+    return unitThreeValue;
+}
+
+double convertUnitOneToUnitFour (double unitOneValue){
+    double unitFourValue;
+    unitFourValue = unitOneValue*1094;
+    return unitFourValue;
+}
+
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentController;
+@property (weak, nonatomic) IBOutlet UILabel *outputField;
 
 @end
 
 @implementation ViewController
+- (IBAction)updateButton:(id)sender {
+    NSMutableString *buf = [NSMutableString new];
+    
+    double userInput =[self.inputField.text doubleValue];
+    
+    if(self.segmentController.selectedSegmentIndex == 0){
+        double unitTwoValue = convertUnitOneToUnitTwo(userInput);
+        [buf appendString:[@(unitTwoValue) stringValue]];
+    }
+    else if(self.segmentController.selectedSegmentIndex == 1){
+        double unitThreeValue = convertUnitOneToUnitThree(userInput);
+        [buf appendString:[@(unitThreeValue) stringValue]];
+    }
+    else{
+        double unitFourValue = convertUnitOneToUnitFour(userInput);
+        [buf appendString:[@(unitFourValue) stringValue]];
+    }
+    
+    self.outputField.text = buf;
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
